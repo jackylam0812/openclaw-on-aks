35ea1cad7ff0f2f8cca7bae81094f7aeff099ce7
Jacky <jacky@JackydeMac-mini.local>
2026-03-13 16:34:21 +0800

    fix: use helm upgrade --install for ingress-nginx to allow re-runs

--- install.sh
+++ install.sh
@@ -276,7 +276,7 @@ deploy_portals() {
   kubectl get ns ingress-nginx >/dev/null 2>&1 || \
     helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx && \
     helm repo update && \
-    helm install ingress-nginx ingress-nginx/ingress-nginx \
+    helm upgrade --install ingress-nginx ingress-nginx/ingress-nginx \
       --namespace ingress-nginx --create-namespace \
       --set controller.service.type=LoadBalancer
 
